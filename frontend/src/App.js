85d7ee45305a7fc26df9d1ebd21bfd503ebce2d8
Cameron Berube <cameron.berube@protonmail.com>
2020-12-17 11:54:07 -0500

    changed it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,7 +41,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Cam!
+          Learn React OK!
         </a>
         <div>
           <button onClick={getInfo}>
